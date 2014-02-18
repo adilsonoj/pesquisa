355fc5837b6f562d29eacd27e7b089957406fa38
Adilson Oliveira <adilsonoj@yahoo.com.br>
2014-02-18 15:36:21 -0300

    criado verifica_browser.php e refatorado valida.php

--- valida.php
+++ valida.php
@@ -3,24 +3,25 @@ session_start();
 require ("conexao.php");
 ini_set( 'default_charset', 'utf-8');
 $usuario = $_REQUEST['usuario'];
+$senha = $_REQUEST['senha'];
 
 	
 		
-		$sql = oci_parse($ora_conn,"SELECT cod_usr_bd FROM dsamger.ge_usr_bd where cod_usr_bd='$usuario'");
-		OCI_Execute($sql);
-		if (oci_fetch($sql))
+		$sql_conexao = oci_parse($ora_conn,"SELECT login FROM usuario WHERE login='$usuario' AND senha='$senha'");
+		OCI_Execute($sql_conexao);
+		if (oci_fetch($sql_conexao))
 		{   
 		 
 			$_SESSION['usuario']=$usuario; 
 			header("location: relatorios.php");
 		}
 		else
-		{?> 
-			
-
-
+		{
+?> 
 			<script type="text/javascript">
 				alert("Usuário ou senha inválida!");
 				window.location="login.php";
 			</script>
-	<?}?>
+<?
+		}
+?>
