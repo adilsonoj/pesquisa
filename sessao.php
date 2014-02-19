62bb78cace2a317defe57e184856344bd1368c3c
Mateus Gregorio <mateusgreg.ufrj@gmail.com>
2014-02-19 17:38:22 -0300

    Refactor: Tornando as mensagens ao usuario mais amigaveis.

--- sessao.php
+++ sessao.php
@@ -1,8 +1,10 @@
-<?php
-session_start();
-if (!isset($_SESSION['usuario']))
-{
-		header("location: login.php");
-}
-		
+<?
+	session_start();
+	
+	if (!isset($_SESSION['usuario'])) {
+		echo "<script language=\"javascript\">";
+			echo "alert(\"Sua sessão expirou ou você está tentando acessar diretamente a área de administração do site.\\nPor favor, efetue o seu login antes de prosseguir.\");";
+			echo "location.href=\"login.php\";";
+		echo "</script>";
+	}	
 ?>
