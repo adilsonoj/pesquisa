52a439e2fcf865e025c55c442e4f27435a03a293
Adilson Oliveira <adilsonoj@yahoo.com.br>
2014-02-21 13:36:42 -0300

    primeira versao completa

--- verifica_browser.php
+++ verifica_browser.php
@@ -5,7 +5,7 @@
 		$browser = 'Internet Explorer';
 
 		echo "<script type='text/javascript'>";
-   	 		echo "alert('Não é indicada a utilização do navegador ".$browser. "\\nPor favor, utilize o navegador Mozilla Firefox para obter uma melhor experiência.');";
+   	 		echo "alert('Não é indicada a utilização do navegador ".$browser. "\\nPorfavor, utilize o navegador Mozilla Firefox para obter uma melhor experiência.');";
 	  	echo "</script>";
   	}
 ?>
