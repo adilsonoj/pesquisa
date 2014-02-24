87c32158eb09f58ec2b24dd63b7283fdbc0891cf
Adilson Oliveira <adilsonoj@yahoo.com.br>
2014-02-24 15:26:39 -0300

    implementado controle de sessao

--- grafico_media.php
+++ grafico_media.php
@@ -1,46 +1,31 @@
 <?php
-
 require("valida_sessao_adm.php");
 require("conexao.php");
 require("classes/phplot.php");
 
-//instanciando classe
 $grafico = new PHPlot(800,600);
 
-//$grafico->SetTitle("Média do grau por questão");
-//$grafico->SetXTitle("QUESTÃO");
-//$grafico->SetYTitle("MÉDIA");
+$dados_grafico = array();
 
-//$grafico->SetLegend("Média");
-
-//$grafico->SetXLabel("MÉDIA");
-
-
-$array = array();
 $sql_pergunta = oci_parse($ora_conn,"SELECT * FROM pergunta ORDER BY numero");
-			oci_execute($sql_pergunta);
+oci_execute($sql_pergunta);
 
-			while ($row_pergunta = oci_fetch_assoc($sql_pergunta)) {
-				$id_pergunta = $row_pergunta['ID'];
+	while ($row_pergunta = oci_fetch_assoc($sql_pergunta)) {
+		$id_pergunta = $row_pergunta['ID'];
 
-				$sql_media = oci_parse($ora_conn, "SELECT AVG(A.ALTERNATIVA) MEDIA FROM RESPOSTA_FECHADA R, ALTERNATIVA A WHERE R.ID_PERGUNTA = $id_pergunta AND R.ID_ALTERNATIVA_RESPOSTA = A.ID");
-				oci_execute($sql_media);
-				$row_media = oci_fetch_assoc($sql_media);
-				
-				if (!$row_media['MEDIA']==NULL){
-					array_push($array, array($id_pergunta, $row_media['MEDIA']));
-				}
+		$sql_media = oci_parse($ora_conn, "SELECT AVG(A.ALTERNATIVA) MEDIA FROM RESPOSTA_FECHADA R, ALTERNATIVA A WHERE R.ID_PERGUNTA = $id_pergunta AND R.ID_ALTERNATIVA_RESPOSTA = A.ID");
+		oci_execute($sql_media);
+		$row_media = oci_fetch_assoc($sql_media);
+		
+		if (!$row_media['MEDIA']==NULL){
+			array_push($dados_grafico, array($id_pergunta, $row_media['MEDIA']));
+		}
 
-			}
+	}
 
-
-$grafico->SetDataValues($array);
-
+$grafico->SetDataValues($dados_grafico);
 $grafico->SetImageBorderType('plain');
- 
 $grafico->SetPlotType("bars");
-
 $grafico->DrawGraph();
-
 
 ?>
