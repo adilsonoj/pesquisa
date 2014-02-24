9c62bc3539f8158e911c813ca239124b74f1db90
Adilson Oliveira <adilsonoj@yahoo.com.br>
2014-02-24 14:40:05 -0300

    implementado grafico de medias

---
+++ exibe_grafico_media.php
@@ -0,0 +1,31 @@
+<?
+	require("valida_sessao_adm.php");
+?>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Pesquisa de Clima Organizacional - Relatórios</title>
+	
+	<link rel="stylesheet" href="css/estilo.css">
+
+
+</head>
+
+<body>
+	<img src="img/header.gif" alt="DSAM">
+
+	<hr id="top"/>
+	<h2>Pesquisa de Clima Organizacional</h2>
+	<hr id="bottom"/>
+
+<div id="grafico">
+	<h4>Gráfico de Médias por questão</h4>
+	<img src="grafico_media.php" />
+
+	
+	<span id="eixoY">MÉDIA</span>
+	<span id="eixoX">QUESTÃO</span>
+</div>
+
+</body>
+</html>
---
+++ grafico_media.php
@@ -0,0 +1,45 @@
+<?php
+//Importando biblioteca
+require("conexao.php");
+require("classes/phplot.php");
+
+//instanciando classe
+$grafico = new PHPlot(800,600);
+
+//$grafico->SetTitle("Média do grau por questão");
+//$grafico->SetXTitle("QUESTÃO");
+//$grafico->SetYTitle("MÉDIA");
+
+//$grafico->SetLegend("Média");
+
+//$grafico->SetXLabel("MÉDIA");
+
+
+$array = array();
+$sql_pergunta = oci_parse($ora_conn,"SELECT * FROM pergunta ORDER BY numero");
+			oci_execute($sql_pergunta);
+
+			while ($row_pergunta = oci_fetch_assoc($sql_pergunta)) {
+				$id_pergunta = $row_pergunta['ID'];
+
+				$sql_media = oci_parse($ora_conn, "SELECT AVG(A.ALTERNATIVA) MEDIA FROM RESPOSTA_FECHADA R, ALTERNATIVA A WHERE R.ID_PERGUNTA = $id_pergunta AND R.ID_ALTERNATIVA_RESPOSTA = A.ID");
+				oci_execute($sql_media);
+				$row_media = oci_fetch_assoc($sql_media);
+				
+				if (!$row_media['MEDIA']==NULL){
+					array_push($array, array($id_pergunta, $row_media['MEDIA']));
+				}
+
+			}
+
+
+$grafico->SetDataValues($array);
+
+$grafico->SetImageBorderType('plain');
+ 
+$grafico->SetPlotType("bars");
+
+$grafico->DrawGraph();
+
+
+?>
