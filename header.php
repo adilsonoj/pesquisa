c01e8e2c7414924ab8eef29b46706c5c6a0238f3
Adilson Oliveira <adilsonoj@yahoo.com.br>
2014-02-26 10:47:13 -0300

    inserido meta tags

--- header.php
+++ header.php
@@ -3,6 +3,11 @@
 <html>
 <head>
 	<meta charset="utf-8">
+	<meta name="author" content="SC Mateus" />
+	<meta name="author" content="CB-ET Adilson" />
+	<meta name="copyright" content="© 2015 DSAM" />
+	<meta name="description" content="Pesquisa de Clima Organizacional - DSAM" />
+
 	<title>Pesquisa de Clima Organizacional</title>
 	<script src="js/jquery.js"></script>
 	<script src="js/script.js"></script>
