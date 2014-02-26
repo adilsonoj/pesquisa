d51e818f18415869752c21e4539df36176d6cabd
Adilson Oliveira <adilsonoj@yahoo.com.br>
2014-02-26 14:23:21 -0300

    corrigido footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 
 		<div id="footer">
-			Copyright &copy; 2014 - DSAM - Diretoria de Sistema de Armas da Marinha	
+			Copyright &copy; 2014 - DSAM - Diretoria de Sistemas de Armas da Marinha	
 		</div>
 
 	</body>
